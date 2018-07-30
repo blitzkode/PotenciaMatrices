185e3d2fb88f8bc2f1fcd4c4c91356e002133fa5
Benny Cisneros <benycisneros@gmail.com>
2018-07-29 23:32:11 -0500

    Creación del proyecto

---
+++ PotenciaMatrices/src/potenciamatrices/Matrices.java
@@ -0,0 +1,17 @@
+package potenciamatrices;
+
+public class Matrices {
+    
+    public static double[][] potencia(double[][] matriz, int exponente) {
+        double[][] resultado = null;
+        
+        return resultado;
+    }
+    
+    public static double[][] multiplicar(double[][] a, double[][] b) {
+        double[][] resultado = null;
+        
+        return resultado;
+    }
+
+}
